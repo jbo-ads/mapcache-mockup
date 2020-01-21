49e7d4ea1e9ed9ce3830cd6c378098ebafad680f
jbo-ads <jbo-ads@laposte.net>
2020-01-21 17:55:06 +0100

    Set configuration optimizations

--- bin/conf-mapcache-catalog.sh
+++ bin/conf-mapcache-catalog.sh
@@ -46,8 +46,23 @@ cat <<-EOF > ${cfgdir}/catalog.xml
 	EOF
 
 sed '/assembly_type/s:^:<assembly_threaded_fetching>true</assembly_threaded_fetching>\n:' ${cfgdir}/catalog.xml > ${cfgdir}/catalog-amt.xml
+
 sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog.xml > ${cfgdir}/catalog-mt.xml
 sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-amt.xml > ${cfgdir}/catalog-mtamt.xml
+
+sed '/<\/validate_query>/s#^#AND miny \&lt;= :maxy AND maxy \&gt;= :miny AND minx \&lt;= :maxx AND maxx \&gt;= :minx\n#' ${cfgdir}/catalog.xml > ${cfgdir}/catalog-geo.xml
+sed '/assembly_type/s:^:<assembly_threaded_fetching>true</assembly_threaded_fetching>\n:' ${cfgdir}/catalog-geo.xml > ${cfgdir}/catalog-amtgeo.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-geo.xml > ${cfgdir}/catalog-mtgeo.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-amtgeo.xml > ${cfgdir}/catalog-mtamtgeo.xml
+
+sed '/assembly_type/s:^:<wms_querybymap>true</wms_querybymap>\n:' ${cfgdir}/catalog.xml > ${cfgdir}/catalog-qbm.xml
+sed '/assembly_type/s:^:<wms_querybymap>true</wms_querybymap>\n:' ${cfgdir}/catalog-geo.xml > ${cfgdir}/catalog-qbmgeo.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-qbm.xml > ${cfgdir}/catalog-mtqbm.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-qbmgeo.xml > ${cfgdir}/catalog-mtqbmgeo.xml
+sed '/assembly_type/s:^:<wms_querybymap>true</wms_querybymap>\n:' ${cfgdir}/catalog-amt.xml > ${cfgdir}/catalog-amtqbm.xml
+sed '/assembly_type/s:^:<wms_querybymap>true</wms_querybymap>\n:' ${cfgdir}/catalog-amtgeo.xml > ${cfgdir}/catalog-amtqbmgeo.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-amtqbm.xml > ${cfgdir}/catalog-mtamtqbm.xml
+sed '$s:^:<threaded_fetching>true</threaded_fetching>\n:' ${cfgdir}/catalog-amtqbmgeo.xml > ${cfgdir}/catalog-mtamtqbmgeo.xml
 
 cat <<-EOF > /etc/apache2/conf-enabled/catalog.conf
 	<IfModule mapcache_module>
@@ -55,6 +70,18 @@ cat <<-EOF > /etc/apache2/conf-enabled/catalog.conf
 		MapCacheAlias "/catalog-amt" "${cfgdir}/catalog-amt.xml"
 		MapCacheAlias "/catalog-mt" "${cfgdir}/catalog-mt.xml"
 		MapCacheAlias "/catalog-mtamt" "${cfgdir}/catalog-mtamt.xml"
+		MapCacheAlias "/catalog-geo" "${cfgdir}/catalog-geo.xml"
+		MapCacheAlias "/catalog-mtgeo" "${cfgdir}/catalog-mtgeo.xml"
+		MapCacheAlias "/catalog-amtgeo" "${cfgdir}/catalog-amtgeo.xml"
+		MapCacheAlias "/catalog-mtamtgeo" "${cfgdir}/catalog-mtamtgeo.xml"
+		MapCacheAlias "/catalog-qbm" "${cfgdir}/catalog-qbm.xml"
+		MapCacheAlias "/catalog-mtqbm" "${cfgdir}/catalog-mtqbm.xml"
+		MapCacheAlias "/catalog-qbmgeo" "${cfgdir}/catalog-qbmgeo.xml"
+		MapCacheAlias "/catalog-mtqbmgeo" "${cfgdir}/catalog-mtqbmgeo.xml"
+		MapCacheAlias "/catalog-amtqbm" "${cfgdir}/catalog-amtqbm.xml"
+		MapCacheAlias "/catalog-mtamtqbm" "${cfgdir}/catalog-mtamtqbm.xml"
+		MapCacheAlias "/catalog-amtqbmgeo" "${cfgdir}/catalog-amtqbmgeo.xml"
+		MapCacheAlias "/catalog-mtamtqbmgeo" "${cfgdir}/catalog-mtamtqbmgeo.xml"
 	</IfModule>
 	EOF
 
@@ -79,6 +106,16 @@ cat <<-EOF > /var/www/html/ol/catalog.js
 		})
 	});
 	layers.unshift(catalogbm)
+	var catalogbmmtgeo = new ol.layer.Tile({
+		title: 'Image Catalog (with basemap) [geographical filter]',
+		type: 'base',
+		visible: false,
+		source: new ol.source.TileWMS({
+			url: 'http://'+location.host+'/catalog-mtgeo?',
+			params: {'LAYERS': 'base,catalog', 'VERSION': '1.1.1'}
+		})
+	});
+	layers.unshift(catalogbmmtgeo)
 	var catalogbmamt = new ol.layer.Tile({
 		title: 'Image Catalog (with basemap) [multi-threaded subtiles]',
 		type: 'base',
